64bca72f091cf36ad48a292bd14c668f338863c2
My name <akshayhallagade2612@gmail.com>
2020-12-30 20:09:00 +0530

    update
    30/12/2020

---
+++ Python/Practice code/test/1.py
@@ -0,0 +1,5 @@
+str = "ABCDE"
+n = 1
+while n < 6:
+    print(n*(str[n-1]+" "))
+    n = n+1
---
+++ Python/Practice code/test/2.py
@@ -0,0 +1,6 @@
+# import math
+n = float(input())*100
+if n > int(n):
+    print(float(n+1/100))
+else:
+    print(n/100)
