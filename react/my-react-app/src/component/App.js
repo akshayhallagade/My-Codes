4b7305f6477a7c56ea70906fe9424e3b5669b54d
Akshay Hallagade <akshayhallagade2612@gmail.com>
2024-02-07 13:16:06 +0530

    new React file added

---
+++ react/my-react-app/src/component/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>AKasdsdfasnfdfsfmlsdfmopsfsdmfl;dmfsdnpfn</p>
+    </>
+  );
+}
+export default App;
